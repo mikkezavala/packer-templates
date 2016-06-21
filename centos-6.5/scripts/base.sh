4af631e912787d44d6b7cd86b0371b83519c5802
Miguel Zavala <miguel@mikkezavala.com>
2016-06-21 16:21:22 -0700

    Installed Puppet Centos 6.4 & 6.5

--- centos-6.5/scripts/base.sh
+++ centos-6.5/scripts/base.sh
@@ -1,3 +1,31 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
+yum -y -q install gcc make gcc-c++ kernel-devel-`uname -r` \
+	git wget zlib zlib-devel libyaml libyaml-devel readline readline-devel libffi-devel \
+	openssl openssl-devel perl bzip2 autoconf automake libtool bison iconv-devel
 
+
+yum -y -q remove ruby ruby-devel
+mkdir /tmp/ruby-build && cd /tmp/ruby-build
+
+wget http://pyyaml.org/download/libyaml/yaml-0.1.6.tar.gz
+tar xzvf yaml-0.1.6.tar.gz
+cd yaml-0.1.6
+./configure
+make
+make install
+
+wget http://ftp.ruby-lang.org/pub/ruby/ruby-1.9.3-p551.tar.gz
+tar zxf ruby-1.9.3-p551.tar.gz
+cd ruby-1.9.3-p551
+./configure
+make
+make install
+
+echo -e '\nexport PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+source ~/.bashrc
+ 
+gem update --system
+gem install bundler
+gem install puppet --no-ri --no-rdoc
+ 
+cd ~/ && rm -rf /tmp/ruby-build
